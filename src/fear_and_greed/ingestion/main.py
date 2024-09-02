82f1ca1a4e4c399efbf32d9f92592e73453e0ddf
d0ndada <pintoavila09@gmail.com>
2024-09-02 14:17:45 +0200

    perf: adding logic for cloud functions, pub/sub for cloud scheduler

--- src/fear_and_greed/ingestion/main.py
+++ src/fear_and_greed/ingestion/main.py
@@ -1,20 +1,34 @@
+from google.cloud import pubsub_v1
 from fear_and_greed.ingestion.fetch_fear_greed_data import fetch_fear_greed_data
 from shared_functions import bigquery_raw_data_table, stream_data_to_bigquery, bigquery_client
+from fastapi import FastAPI, HTTPException
 import logging
 
+api_url = "https://api.alternative.me/fng/?limit=1"
 
-def main():
-    api_url = "https://api.alternative.me/fng/?limit=10"
+app = FastAPI()
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path("shabubsinc", "trigger-clean-fear-greed")
+
+
+@app.post("/ingest/fear-greed/raw")
+def ingest_fear_greed_raw():
     fear_greed_data = fetch_fear_greed_data(api_url)
-    
-    raw_fear_greed_data = bigquery_raw_data_table(bigquery_client=bigquery_client, dataset_id='shabubsinc_db', table_id='raw_daily_fear_greed_data',api_data=fear_greed_data)
+
+    raw_fear_greed_data = bigquery_raw_data_table(
+        bigquery_client=bigquery_client, dataset_id='shabubsinc_db', table_id='raw_daily_fear_greed_data', api_data=fear_greed_data)
 
     try:
-        stream_data_to_bigquery(bigquery_client=bigquery_client, data=raw_fear_greed_data, project_id='shabubsinc', dataset_id='shabubsinc_db', table_id='raw_daily_fear_greed_data')
-    
+        stream_data_to_bigquery(bigquery_client=bigquery_client, data=raw_fear_greed_data,
+                                project_id='shabubsinc', dataset_id='shabubsinc_db', table_id='raw_daily_fear_greed_data')
+        publisher.publish(
+            topic_path, b"Trigger clean processing for Fear & Greed")
+        return {"status": "success"}
+
     except Exception as e:
         logging.error(f"Failed to stream data to BigQuery: {e}")
+        raise HTTPException(status_code=500, detail="Data ingestion failed")
 
 
 if __name__ == "__main__":
-    main()
+    ingest_fear_greed_raw()
--- src/fear_and_greed/processing/main.py
+++ src/fear_and_greed/processing/main.py
@@ -1,19 +1,31 @@
 from storage.fetch_data_from_db import get_all_data_from_bigquery
 from shared_functions import stream_data_to_bigquery, bigquery_client
 from fear_and_greed.processing.process_fear_greed_data import ensure_bigquery_fear_greed_table, extract_required_fields
+from fastapi import FastAPI, HTTPException
 import logging
 
-def fear_greed(): 
+app = FastAPI()
 
-    fear_greed_raw_data = get_all_data_from_bigquery(bigquery_client=bigquery_client,dataset_id='shabubsinc_db',table_id='raw_daily_fear_greed_data')
-    ensure_bigquery_fear_greed_table(bigquery_client=bigquery_client, dataset_id='shabubsinc_db',table_id='clean_daily_fear_greed_data')
-   
+
+@app.post("/ingest/fear-greed/clean")
+def ingest_fear_greed_clean():
+
+    fear_greed_raw_data = get_all_data_from_bigquery(
+        bigquery_client=bigquery_client, dataset_id='shabubsinc_db', table_id='raw_daily_fear_greed_data')
+    ensure_bigquery_fear_greed_table(
+        bigquery_client=bigquery_client, dataset_id='shabubsinc_db', table_id='clean_daily_fear_greed_data')
+
     structured_data = extract_required_fields(fear_greed_raw_data)
-    
+
     try:
-        stream_data_to_bigquery(bigquery_client=bigquery_client, data=structured_data, project_id='shabubsinc', dataset_id='shabubsinc_db', table_id='clean_daily_fear_greed_data')
+        stream_data_to_bigquery(bigquery_client=bigquery_client, data=structured_data,
+                                project_id='shabubsinc', dataset_id='shabubsinc_db', table_id='clean_daily_fear_greed_data')
+        return {"status": "success"}
+
     except Exception as e:
         logging.error(f"Failed to stream fear and greed data to BigQuery: {e}")
+        raise HTTPException(status_code=500, detail="Data processing failed")
+
 
 if __name__ == "__main__":
-    fear_greed()
+    ingest_fear_greed_clean()
