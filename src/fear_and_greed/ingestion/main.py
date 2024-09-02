4daadbbfdc353a8bd6f1da43b10f78d459e6bcba
Hussein Chukair <hussein.chukair@gmail.com>
2024-09-02 12:29:03 +0200

    Changed imports to accomodate for putting some functions in /shared_functions

--- src/fear_and_greed/ingestion/main.py
+++ src/fear_and_greed/ingestion/main.py
@@ -1,16 +1,16 @@
-from config import api_url
 from fear_and_greed.ingestion.fetch_fear_greed_data import fetch_fear_greed_data
-from storage.store_in_db import bigquery_raw_data_table, stream_data_to_bigquery, client
+from shared_functions import bigquery_raw_data_table, stream_data_to_bigquery, bigquery_client
 import logging
 
 
 def main():
+    api_url = "https://api.alternative.me/fng/?limit=10"
     fear_greed_data = fetch_fear_greed_data(api_url)
     
-    raw_fear_greed_data = bigquery_raw_data_table(client=client, dataset_id='shabubsinc_db', table_id='raw_daily_fear_greed_data',api_data=fear_greed_data)
+    raw_fear_greed_data = bigquery_raw_data_table(bigquery_client=bigquery_client, dataset_id='shabubsinc_db', table_id='raw_daily_fear_greed_data',api_data=fear_greed_data)
 
     try:
-        stream_data_to_bigquery(client=client, data=raw_fear_greed_data, project_id='shabubsinc', dataset_id='shabubsinc_db', table_id='raw_daily_fear_greed_data')
+        stream_data_to_bigquery(bigquery_client=bigquery_client, data=raw_fear_greed_data, project_id='shabubsinc', dataset_id='shabubsinc_db', table_id='raw_daily_fear_greed_data')
     
     except Exception as e:
         logging.error(f"Failed to stream data to BigQuery: {e}")
--- src/ohlc/processing/main.py
+++ src/ohlc/processing/main.py
@@ -1,16 +1,15 @@
-from storage.fetch_data_from_db import get_all_data_from_bigquery
-from storage.store_in_db import stream_data_to_bigquery, client
+from shared_functions import stream_data_to_bigquery, bigquery_client, get_raw_data_from_bigquery
 from ohlc.processing.process_ohlc_data import ensure_bigquery_ohlc_table
 import logging
 
 def ohlc(): 
 
-    ohlc_raw_data = get_all_data_from_bigquery(client=client,dataset_id='shabubsinc_db',table_id='raw_hourly_ohlc_data')
+    ohlc_raw_data = get_raw_data_from_bigquery(bigquery_client=bigquery_client,dataset_id='shabubsinc_db',table_id='raw_hourly_ohlc_data')
 
-    ensure_bigquery_ohlc_table(client=client, dataset_id='shabubsinc_db',table_id='clean_hourly_ohlc_data')
+    ensure_bigquery_ohlc_table(bigquery_client=bigquery_client, dataset_id='shabubsinc_db',table_id='clean_hourly_ohlc_data')
 
     try:
-        stream_data_to_bigquery(client=client, data=ohlc_raw_data, project_id='shabubsinc', dataset_id='shabubsinc_db', table_id='clean_hourly_ohlc_data')
+        stream_data_to_bigquery(bigquery_client=bigquery_client, data=ohlc_raw_data, project_id='shabubsinc', dataset_id='shabubsinc_db', table_id='clean_hourly_ohlc_data')
     except Exception as e:
         logging.error(f"Failed to stream hourly ohlc data to BigQuery: {e}")
 
