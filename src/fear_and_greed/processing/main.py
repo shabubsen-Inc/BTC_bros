03bde9b19898f3497eaf35d599a333cde5931248
d0ndada <pintoavila09@gmail.com>
2024-09-05 19:21:38 +0200

    perf: added improved logic for pub/sub message and flow of function

--- src/fear_and_greed/processing/main.py
+++ src/fear_and_greed/processing/main.py
@@ -12,58 +12,73 @@ import json
 from google.cloud import pubsub_v1
 from fastapi import FastAPI, HTTPException
 import logging
+from starlette.requests import Request
 
 app = FastAPI()
 
 publisher = pubsub_v1.PublisherClient()
-topic_path = publisher.topic_path("shabubsinc","trigger-consume")
+topic_path = publisher.topic_path("shabubsinc", "trigger-consume")
 
 
 @app.post("/ingest/fear-greed/clean")
-def ingest_fear_greed_clean():
-
-    fear_greed_raw_data = get_raw_data_from_bigquery(
-        bigquery_client=bigquery_client,
-        dataset_id="shabubsinc_db",
-        table_id="raw_daily_fear_greed_data",
-    )
-    ensure_bigquery_fear_greed_table(
-        bigquery_client=bigquery_client,
-        dataset_id="shabubsinc_db",
-        table_id="clean_daily_fear_greed_data",
-    )
-    structured_data = extract_required_fields(fear_greed_raw_data)
-
-    clean_data = filter_duplicates_fear_greed(
-        bigquery_client=bigquery_client,
-        dataset_id="shabubsinc_db",
-        table_id="clean_daily_fear_greed_data",
-        raw_data=structured_data,
-    )
-
+async def ingest_fear_greed_clean(request: Request):
     try:
-        stream_data_to_bigquery(
-            bigquery_client=bigquery_client,
-            data=clean_data,
-            project_id="shabubsinc",
-            dataset_id="shabubsinc_db",
-            table_id="clean_daily_fear_greed_data",
-        )
-        
-        message = json.dumps({
-            "status": "completed",
-            "source": "fear_greed_clean",
-            "table": "clean_daily_fear_greed_data"
-        }).encode("utf-8")
-        
-        publisher.publish(topic_path, message)
-        publisher.result()
-        logging.info("Published message to trigger consumption process.")
+        request_body = await request.json()
+        message_data = json.loads(request_body["message"]["data"])
 
-        return {"status": "success"}
+        source = message_data.get("source")
+        table = message_data.get("table")
+
+        if source == "fear_greed_raw":
+            logging.info(f"Processing clean data for {source}, table: {table}")
+
+            fear_greed_raw_data = get_raw_data_from_bigquery(
+                bigquery_client=bigquery_client,
+                dataset_id="shabubsinc_db",
+                table_id=table,
+            )
+            ensure_bigquery_fear_greed_table(
+                bigquery_client=bigquery_client,
+                dataset_id="shabubsinc_db",
+                table_id="clean_daily_fear_greed_data",
+            )
+            structured_data = extract_required_fields(fear_greed_raw_data)
+
+            clean_data = filter_duplicates_fear_greed(
+                bigquery_client=bigquery_client,
+                dataset_id="shabubsinc_db",
+                table_id="clean_daily_fear_greed_data",
+                raw_data=structured_data,
+            )
+
+            stream_data_to_bigquery(
+                bigquery_client=bigquery_client,
+                data=clean_data,
+                project_id="shabubsinc",
+                dataset_id="shabubsinc_db",
+                table_id="clean_daily_fear_greed_data",
+            )
+            logging.info("Fear & Greed data cleaned and streamed successfully.")
+
+            message = json.dumps(
+                {
+                    "status": "completed",
+                    "source": "fear_greed_clean",
+                    "table": "clean_daily_fear_greed_data",
+                }
+            ).encode("utf-8")
+
+            future = publisher.publish(topic_path, message)
+            await future.result()
+            logging.info("Published message to trigger consumption process.")
+
+            return {"status": "success"}
+        else:
+            logging.warning(f"Message source {source} is not relevant, ignoring.")
+            return {"status": "ignored"}
 
     except Exception as e:
-        logging.error(f"Failed to stream fear and greed data to BigQuery: {e}")
+        logging.error(f"Failed to process Fear & Greed data: {e}")
         raise HTTPException(status_code=500, detail="Data processing failed")
 
 
--- src/ohlc/processing/main.py
+++ src/ohlc/processing/main.py
@@ -9,57 +9,71 @@ from ohlc.processing.process_ohlc_data import ensure_bigquery_ohlc_table
 from google.cloud import pubsub_v1
 import logging
 from fastapi import FastAPI, HTTPException
-
+from starlette.requests import Request
 
 app = FastAPI()
 
 publisher = pubsub_v1.PublisherClient()
-topic_path = publisher.topic_path("shabubsinc","trigger-consume")
+topic_path = publisher.topic_path("shabubsinc", "trigger-consume")
+
 
 @app.post("/ingest/ohlc/clean")
-def ingest_ohlc_clean():
-
-    ohlc_raw_data = get_raw_data_from_bigquery(
-        bigquery_client=bigquery_client,
-        dataset_id="shabubsinc_db",
-        table_id="raw_hourly_ohlc_data",
-    )
-
-    ensure_bigquery_ohlc_table(
-        bigquery_client=bigquery_client,
-        dataset_id="shabubsinc_db",
-        table_id="clean_hourly_ohlc_data",
-    )
-    clean_data = filter_duplicates_ohlc(
-        bigquery_client=bigquery_client,
-        dataset_id="shabubsinc_db",
-        table_id="clean_hourly_ohlc_data",
-        raw_data=ohlc_raw_data,
-    )
+async def ingest_ohlc_clean(request: Request):
     try:
-        stream_data_to_bigquery(
-            bigquery_client=bigquery_client,
-            data=clean_data,
-            project_id="shabubsinc",
-            dataset_id="shabubsinc_db",
-            table_id="clean_hourly_ohlc_data",
-        )
+        request_body = await request.json()
+        message_data = json.loads(request_body["message"]["data"])
 
-        message = json.dumps({
-            "status": "completed",
-            "source": "ohlc_clean",
-            "table": "clean_hourly_ohlc_data"
-        }).encode("utf-8")
-        
-        publisher.publish(topic_path, message)
-        publisher.result()
-        logging.info("Published message to trigger consumption process.")
+        source = message_data.get("source")
+        table = message_data.get("table")
 
+        if source == "ohlc_raw":
+            logging.info(f"Processing clean data for {source}, table: {table}")
 
-        return {"status": "success"}
+            ohlc_raw_data = get_raw_data_from_bigquery(
+                bigquery_client=bigquery_client,
+                dataset_id="shabubsinc_db",
+                table_id=table,
+            )
+
+            ensure_bigquery_ohlc_table(
+                bigquery_client=bigquery_client,
+                dataset_id="shabubsinc_db",
+                table_id="clean_hourly_ohlc_data",
+            )
+            clean_data = filter_duplicates_ohlc(
+                bigquery_client=bigquery_client,
+                dataset_id="shabubsinc_db",
+                table_id="clean_hourly_ohlc_data",
+                raw_data=ohlc_raw_data,
+            )
+            stream_data_to_bigquery(
+                bigquery_client=bigquery_client,
+                data=clean_data,
+                project_id="shabubsinc",
+                dataset_id="shabubsinc_db",
+                table_id="clean_hourly_ohlc_data",
+            )
+            logging.info("OHLC cleaned and streamed successfully.")
+
+            message = json.dumps(
+                {
+                    "status": "completed",
+                    "source": "ohlc_clean",
+                    "table": "clean_hourly_ohlc_data",
+                }
+            ).encode("utf-8")
+
+            future = publisher.publish(topic_path, message)
+            await future.result()
+            logging.info("Published message to trigger consumption process.")
+
+            return {"status": "success"}
+        else:
+            logging.warning(f"Message source {source} is not relevant, ignoring.")
+            return {"status": "ignored"}
 
     except Exception as e:
-        logging.error(f"Failed to stream hourly ohlc data to BigQuery: {e}")
+        logging.error(f"Failed to process OHLC data: {e}")
         raise HTTPException(status_code=500, detail="Data ingestion failed")
 
 
